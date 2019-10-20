70d9d475ad7776b6b31b8c316bead8f6682e65d5
Edwin <sleepyrainyday@naver.com>
2019-10-20 15:23:26 +0900

    Assert에 예외 체크 추가

--- src/Framework/Assertion/Assert.ts
+++ src/Framework/Assertion/Assert.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import AssertException from './AssertException';
+import { ActionDelegate as Action } from '../Delegate';
 
 export default class Assert 
 {
@@ -28,8 +29,22 @@ export default class Assert
         }
     }
 
-    public static MustThrow<T>(action: function())
+    public static ThrowException(action: Action, msg: string)
     {
-
+        try 
+        {
+            action(null);
+            throw new AssertException("지정한 Action에서 예외가 발생하지 않습니다.");
+        }
+        catch (error)
+        {
+            if(!error.hasOwnProperty("msg"))
+            {
+                throw new AssertException("발생한 예외는 ${msg}를 출력하지 않습니다. : ${error}");
+            }
+            else if(error.msg !== msg){
+                throw new AssertException("발생한 예외 메시지는 예상 메시지 ${msg}와 다릅니다. : ${error.msg}");
+            }
+        }
     }
 }
