bd340b902723f21bfa1116b13c01037ea753d6a9
Edwin <sleepyrainyday@naver.com>
2019-12-15 17:50:02 +0900

    Assert에 Null체크 추가

--- src/Framework/Assertion/Assert.ts
+++ src/Framework/Assertion/Assert.ts
@@ -20,7 +20,23 @@ export class Assert
 
     public static IsEqual<T>(value: T, expected: T, msg: string = "Assert.IsEqual에서 실제 값 ${value}이 예측한 값 ${expected}과 다릅니다.")
     {
-        if(value != expected)
+        if(value !== expected)
+        {
+            throw Error(msg);
+        }
+    }
+
+    public static IsNull<T>(value: T, msg: string = "Null이어야 하는데 Null이 아닙니다.")
+    {
+        if(value !== null)
+        {
+            throw Error(msg);
+        }
+    }
+
+    public static IsNotNull<T>(value: T, msg: string = "Null이 아니어야 하는데 Null입니다.")
+    {
+        if(value === null)
         {
             throw Error(msg);
         }
