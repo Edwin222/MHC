72425cca67762cd3fe83797edc1f110918ed5501
Edwin <sleepyrainyday@naver.com>
2020-01-26 17:58:18 +0900

    유닛 테스트 빌드 스크립트 수정

--- build/UnitTest.sh
+++ build/UnitTest.sh
@@ -1,8 +1,21 @@
+echo "데이터 읽기 경로 지정"
+./plugins/ExcelToJson/ExcelToJson.exe -in "data"
+
+echo "데이터 쓰기 경로 지정"
+./plugins/ExcelToJson/ExcelToJson.exe -out "json"
+
+echo "데이터 컨버트"
+./plugins/ExcelToJson/ExcelToJson.exe
+echo "==============================================="
+
 echo "프로젝트 빌드 시작"
-yarn build
+./node_modules/.bin/webpack
+
+echo "==============================================="
 
 echo "유닛 테스트 시작"
 yarn _jest_test_run
 
+echo "==============================================="
 echo "계속하려면 아무 키나 누르세요."
 read
